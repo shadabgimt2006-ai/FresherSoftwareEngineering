068bd36cc1903b0190b3a3ce9bd2696f26b0a49e
DESKTOP-GF67IHK\admin <shadabgimt2006@gmail.com>
2024-12-25 13:52:41 +0530

    Initial Check In

---
+++ Node-Development/1-samplewebapp/public/js/script.js
@@ -0,0 +1,1 @@
+console.log('Web Application Loaded Successfully');
---
+++ Node-Development/1-samplewebapp/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/homepage.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views/login.html')));
+
+app.post('/register', (req, res) => {
+  console.log('User Registered:', req.body);
+  res.redirect('/');
+});
+
+app.post('/login', (req, res) => {
+  console.log('User Login Attempt:', req.body);
+  res.redirect('/');
+});
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
