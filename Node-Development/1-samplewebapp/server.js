8af2c920608115164d61348bafe24f3e6a8407b7
DESKTOP-GF67IHK\admin <shadabgimt2006@gmail.com>
2024-12-26 13:00:00 +0530

    Code start working

--- Node-Development/1-samplewebapp/server.js
+++ Node-Development/1-samplewebapp/server.js
@@ -10,11 +10,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/homepage.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views/register.html')));
+app.get('/', (req, res) => {
+
+  console.log('Homepage accessed');
+  console.log(path.join(__dirname, 'views/homepage.html'))
+  res.sendFile(path.join(__dirname, 'views/homepage.html'));
+});
+app.get('/register', (req, res) =>{
+  console.log('register accessed');
+  console.log(path.join(__dirname, 'views/register.html'))
+  res.sendFile(path.join(__dirname, 'views/register.html'))
+});
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views/login.html')));
 
-app.post('/register', (req, res) => {
+app.post('/register', (req, res) => {   
   console.log('User Registered:', req.body);
   res.redirect('/');
 });
